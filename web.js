b999e8b5fe00a614136c95fa9d7e711e7e856b8f
Andrey Dyakov <andrey.dyakov@aitudala.kz>
2022-10-05 20:59:46 +0300

    fix listen

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
 // web.js
 const http = require("http");
-const host = 'localhost';
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8000;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end("Ok!");
 };
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+server.listen(port, () => {
+  console.log(`Server is listen port:${port}`);
 });
